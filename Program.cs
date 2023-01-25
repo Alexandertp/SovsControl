8bb2383a9e7f04196c58795cdfb1c504aafc03b2
Mikkel Troels Kongsted <mtkongsted@gmail.com>
2023-01-25 12:06:45 +0100

    just better

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-
-int i = 0;
-while (i < 4) {
-EatMyCPU();
-i++;
-}
-
-void EatMyCPU() {
-int EatI = 0;
-while (EatI < 10000) {
-    i++;
-}
-Console.Writeline("Done!");
+using System;
+					
+public class Program
+{
+	
+	public static void EatMyCPU(int id) {
+		for (var i = 0; i < 10000; i++) {
+			if (i == 69) {
+				Console.WriteLine("Nice");
+			}
+			
+		}
+	}
+	
+	public static void Main()
+	{
+		int i = 0;
+		while (i < 4) {
+		EatMyCPU(i);
+		i++;
+		}
+		Console.WriteLine("Done for real!");
+		
+	}
 }
