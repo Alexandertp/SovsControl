c5ddfa9b160a94553a1cb3491e81130c8879b1e1
Alexandertp <sandertp@gmail.com>
2023-01-25 12:11:57 +0100

    Så det fixet
    :)

--- Program.cs
+++ Program.cs
@@ -1,25 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
 using System;
-					
+
 public class Program
 {
-	
-	public static void EatMyCPU(int id) {
-		for (var i = 0; i < 10000; i++) {
-			if (i == 69) {
-				Console.WriteLine("Nice");
-			}
-			
-		}
-	}
-	
-	public static void Main()
-	{
-		int i = 0;
-		while (i < 4) {
-		EatMyCPU(i);
-		i++;
-		}
-		Console.WriteLine("Done for real!");
-		
-	}
+
+    public static void EatMyCPU(int id)
+    {
+        for (var i = 0; i < 10000; i++)
+        {
+            if (i == 69)
+            {
+                Console.WriteLine("Nice");
+            }
+
+        }
+    }
+
+    public static void Main()
+    {
+        int i = 0;
+        while (i < 4)
+        {
+            EatMyCPU(i);
+            i++;
+        }
+        Console.WriteLine("Done for real!");
+
+    }
 }
